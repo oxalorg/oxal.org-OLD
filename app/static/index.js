85e1f2610ca2e55ae99db6d87d9807ae1a8eae25
Mitesh Shah <mitesh@miteshshah.com>
2016-09-27 20:30:19 +0530

    Compelte index rework. Added c00l terminal tty.

--- app/static/index.js
+++ app/static/index.js
@@ -15,19 +15,61 @@ var inputHandler = function (ans) {
     console.log(ans);
     switch (ans.toLowerCase()) {
         case 'help':
-            arkControl.print("Commands available:\n\t'help'\n\t'ark-info'");
+            arkControl.print("Commands available:");
+            arkControl.print(" -- help");
+            arkControl.print(" -- captain");
+            arkControl.print(" -- about");
+            arkControl.print(" -- navigate");
+            arkControl.print(" -- clear");
+            arkControl.printHTML("<br />");
             break;
-        case 'ark-info':
-            arkControl.print("The ship is currently repairing. Will be ready to sail within a few days.");
+
+        case 'captain':
+            arkControl.printHTML("The capt'n of the ship is <strong style=\"color:white;\"><em>oxalorg</em> &lt;rogue@oxal.org>.</strong>");
+            arkControl.print("He likes to dream that he's an ordinary carbon-based\
+                              life form, but in reality he's a rogue time travelling\
+                              intergalactic ninja who likes cooking and rambling.\
+                              He also thinks he is a badass summoner, excelling in the\
+                              arts of witchcraft and necromancy, and that he\
+                              can raise an army of the dead in a blink of an eye.\
+                              (although I've NEVER seen him do it)");
+            arkControl.printHTML("Find out more about this n00b at <a href=about style=\"color:white;\">oxal.org/about</a>");
+            arkControl.printHTML("<br />");
             break;
+
+        case 'ark':
+            arkControl.print("This is 'ark', oxalorgs home base; and I am his A.I bot.\
+                              Sakura 310. I maintain all the servers and helper\
+                              programs.");
+            arkControl.print("The ship is currently sailing from the Ptolemy nebula to the\
+                              inner regions of CCG-203. You're free to upload yourself to\
+                              our guest chambers for a nice cup of tea.");
+            arkControl.printHTML("<br />");c
+            break;
+
+        case 'navigate':
+            var getAnchor = function (href, text){
+                return '<a target=_blank style="color:inherit" href='+ href +'>'+ text + '</a>';
+            }
+            arkControl.printHTML(getAnchor('about', 'oxal.org/about'));
+            arkControl.printHTML(getAnchor('o', 'oxal.org/o'));
+            arkControl.printHTML(getAnchor('upload', 'oxal.org/upload'));
+            arkControl.printHTML(getAnchor('antisocial', 'oxal.org/antisocial'));
+            arkControl.printHTML(getAnchor('git', 'oxal.org/git') + ' -- Coming soon.');
+            arkControl.printHTML('<a style="color:inherit" href="git">oxal.org/git</a> -- Coming soon.');
+            break;
+
+        case 'clear':
+            arkControl.clear();
+            break;
+
         default:
-            arkControl.print("Hey. Quit bugging around.");
+            arkControl.print("Type H.E.L.P (help). PS: This is not a distress signal.");
     }
     arkControl.input("", inputHandler);
 }
 
 // Main logic
-arkControl.print("Connecting ...");
+arkControl.print("Connecting...");
 arkControl.print("Logged in @ark.");
-arkControl.input("Welcome aboard. Type 'help' to recieve help.", inputHandler);
-
+arkControl.input("Welcome aboard. Type `help` to recieve help.", inputHandler);
--- app/static/terminal.js
+++ app/static/terminal.js
@@ -17,6 +17,7 @@ var Terminal = (function () {
 	}
 
 	var firstPrompt = true;
+	var promptChar = '$';
 	promptInput = function (terminalObj, message, PROMPT_TYPE, callback) {
 		var shouldDisplayInput = (PROMPT_TYPE === PROMPT_INPUT)
 		var inputField = document.createElement('input')
@@ -28,10 +29,14 @@ var Terminal = (function () {
 		inputField.style.opacity = '0'
 		inputField.style.fontSize = '0.2em'
 
-		terminalObj._inputLine.textContent = '$ '
+		terminalObj._inputLine.textContent = promptChar + ' '
 		terminalObj._input.style.display = 'block'
 		terminalObj.html.appendChild(inputField)
 		fireCursorInterval(inputField, terminalObj)
+
+		var cleanInput = function (input) {
+			return input.slice(promptChar.length + 1, input.length)
+		}
 
 		if (message.length) terminalObj.print(PROMPT_TYPE === PROMPT_CONFIRM ? message + ' (y/n)' : message)
 
@@ -63,6 +68,7 @@ var Terminal = (function () {
 				var inputValue = inputField.value
 				if (shouldDisplayInput) terminalObj.print(inputValue)
 				terminalObj.html.removeChild(inputField)
+				inputValue = cleanInput(inputValue)
 				if (typeof(callback) === 'function') {
 					if (PROMPT_TYPE === PROMPT_CONFIRM) {
 						callback(inputValue.toUpperCase()[0] === 'Y' ? true : false)
@@ -108,6 +114,12 @@ var Terminal = (function () {
 		this.print = function (message) {
 			var newLine = document.createElement('div')
 			newLine.textContent = message
+			this._output.appendChild(newLine)
+		}
+
+		this.printHTML = function (message) {
+			var newLine = document.createElement('div')
+			newLine.innerHTML = message
 			this._output.appendChild(newLine)
 		}
 
@@ -174,11 +186,11 @@ var Terminal = (function () {
 		this.html.style.margin = '0'
 		this._innerWindow.style.padding = '10px'
 		this._input.style.margin = '0'
-		this._output.style.margin = '0';
-		this._cursor.style.background = 'white';
-		this._cursor.innerHTML = 'C'; //put something in the cursor..
-		this._cursor.style.display = 'none'; //then hide it
-		this._input.style.display = 'none';
+		this._output.style.margin = '0'
+		this._cursor.style.background = 'white'
+		this._cursor.innerHTML = 'C' //put something in the cursor..
+		this._cursor.style.display = 'none' //then hide it
+		this._input.style.display = 'none'
 	}
 
 	return TerminalConstructor
